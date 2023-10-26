7dc7230f765054ee7e932ec7301b3dbaa1d24d6b
Adam Henault <henault.adam@gmail.com>
2023-10-26 17:01:33 +0200

    Challenge 2 automation

--- Writeup/Challenge 2/exp/dtmf-decoder-master/dtmf.py
+++ Writeup/Challenge 2/exp/dtmf-decoder-master/dtmf.py
@@ -1,81 +1,60 @@
-#!/usr/bin/env python3
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 import argparse
-
-dtmf = {(697, 1209): "1", (697, 1336): "2", (697, 1477): "3", (770, 1209): "4", (770, 1336): "5", (770, 1477): "6", (852, 1209): "7", (852, 1336): "8", (852, 1477): "9", (941, 1209): "*", (941, 1336): "0", (941, 1477): "#", (697, 1633): "A", (770, 1633): "B", (852, 1633): "C", (941, 1633): "D"}
-
-
-parser = argparse.ArgumentParser(description="Extract phone numbers from an audio recording of the dial tones.")
-parser.add_argument("-v", "--verbose", help="show a complete timeline", action="store_true")
-parser.add_argument("-l", "--left", help="left channel only (if the sound is stereo)", action="store_true")
-parser.add_argument("-r", "--right", help="right channel only (if the sound is stereo)", action="store_true")
-parser.add_argument("-d", "--debug", help="show graphs to debug", action="store_true")
-parser.add_argument("-t", type=int, metavar="F", help="acceptable frequency error (in hertz, 20 by default)", default=20)
-parser.add_argument("-i", type=float, metavar='T', help="process by T seconds intervals (0.04 by default)", default=0.04)
-
-parser.add_argument('file', type=argparse.FileType('r'))
-
-args = parser.parse_args()
+from collections import Counter
 
 
-file = args.file.name
-try:
-    fps, data = wavfile.read(file)
-except FileNotFoundError:
-    print ("No such file:", file)
-    exit()
-except ValueError:
-    print ("Impossible to read:", file)
-    print("Please give a wav file.")
-    exit()
+sample_freq = {1722 : "0", 953 : "1", 1012 : "2", 1063 : "3", 1187 : "4", 1247 : "5", 1305 : "6", 1429 : "7", 1486 : "8", 1546 : "9", 1131 : "A", 1363 : "B", 1606 : "C", 1835 : "D", 1664: "E"}
 
+def get_freq(data):
 
-if args.left and not args.right:
-    if len(data.shape) == 2 and data.shape[1] == 2:
-        data = np.array([i[0] for i in data])
-    elif len(data.shape) == 1:
-        print ("Warning: The sound is mono so the -l option was ignored.")
-    else:
-        print ("Warning: The sound is not mono and not stereo ("+str(data.shape[1])+" canals)... so the -l option was ignored.")
+    found_frequencies = []
 
+    duration = len(data)/fps
+    step = int(len(data)//(duration//0.1))
 
-elif args.right and not args.left:
-    if len(data.shape) == 2 and data.shape[1] == 2:
-        data = np.array([i[1] for i in data])
-    elif len(data.shape) == 1:
-        print ("Warning: the sound is mono so the -r option was ignored.")
-    else:
-        print ("Warning: The sound is not mono and not stereo ("+str(data.shape[1])+" canals)... so the -r option was ignored.")
+    for i in range(0, len(data)-step, step):
+        signal = data[i:i+step]
+        
+        frequencies = np.fft.fftfreq(signal.size, d=1/fps)
+        amplitudes = np.fft.fft(signal)
 
-else:
-    if len(data.shape) == 2: 
-        data = data.sum(axis=1) # stereo
+        # Low
+        i_min = np.where(frequencies > 50)[0][0]
+        i_max = np.where(frequencies > 2000)[0][0]
+        
+        freq = frequencies[i_min:i_max]
+        amp = abs(amplitudes.real[i_min:i_max])
 
-precision = args.i
+        lf = freq[np.where(amp == max(amp))[0][0]]
 
-duration = len(data)/fps
+        delta = 20
+        best = 0
 
-step = int(len(data)//(duration//precision))
+        for f in sample_freq:
+            if abs(lf-f) < delta:
+                delta = abs(lf-f)
+                best = f
 
-debug = args.debug
-verbose = args.verbose
-c = ""
+        found_frequencies.append(best)
 
-if debug:
-    print("Warning:\nThe debug mode is very uncomfortable: you need to close each window to continue.\nFeel free to kill the process doing CTRL+C and then close the window.\n")
+    frequencies_count = Counter(found_frequencies)
+    found_freq, _ = frequencies_count.most_common(1)[0]
+    return found_freq
 
-if verbose:
-    print ("0:00 ", end='', flush=True)
+def get_freq_seq(data, debug):
 
-try:
+    found_frequencies = []
+
+    duration = len(data)/fps
+    step = int(len(data)//(duration//0.1))
+
     for i in range(0, len(data)-step, step):
         signal = data[i:i+step]
 
         if debug:
-            plt.subplot(311)
+            plt.subplot(211)
             plt.subplots_adjust(hspace=0.5)
             plt.title("audio (entire signal)")
             plt.plot(data)
@@ -83,95 +62,50 @@ try:
             plt.yticks([])
             plt.axvline(x=i, linewidth=1, color='red')
             plt.axvline(x=i+step, linewidth=1, color='red')
-            plt.subplot(312)
-            plt.title("analysed frame")
-            plt.plot(signal)
-            plt.xticks([])
-            plt.yticks([])
         
         
         frequencies = np.fft.fftfreq(signal.size, d=1/fps)
         amplitudes = np.fft.fft(signal)
 
         # Low
-        # i_min = np.where(frequencies > 0)[0][0]
-        # i_max = np.where(frequencies > 2000)[0][0]
+        i_min = np.where(frequencies > 50)[0][0]
+        i_max = np.where(frequencies > 2000)[0][0]
         
-        freq = frequencies[:]
-        amp = abs(amplitudes.real[:])
+        freq = frequencies[i_min:i_max]
+        amp = abs(amplitudes.real[i_min:i_max])
 
         lf = freq[np.where(amp == max(amp))[0][0]]
 
-        delta = args.t
-        # best = 0
+        delta = 20
+        best = 0
 
-        # for f in [697, 770, 852, 941]:
-        #     if abs(lf-f) < delta:
-        #         delta = abs(lf-f)
-        #         best = f
+        for f in sample_freq:
+            if abs(lf-f) < delta:
+                delta = abs(lf-f)
+                best = f
 
         if debug:
-            plt.subplot(313)
+            plt.subplot(212)
             plt.title("Fourier transform")
             plt.plot(freq, amp)
-            ax = plt.gca()
-            ax.set_ylim([0,200000])
-            plt.yticks([])
             plt.annotate(str(int(lf))+"Hz", xy=(lf, max(amp)))
 
-        # lf = best
-
-        # # High
-        # i_min = np.where(frequencies > 1100)[0][0]
-        # i_max = np.where(frequencies > 2000)[0][0]
-
-        # freq = frequencies[i_min:i_max]
-        # amp = abs(amplitudes.real[i_min:i_max])
-
-        # hf = freq[np.where(amp == max(amp))[0][0]]
-
-        # delta = args.t
-        # best = 0
-
-        # for f in [1209, 1336, 1477, 1633]:
-        #     if abs(hf-f) < delta:
-        #         delta = abs(hf-f)
-        #         best = f
-
-        # if debug:
-        #     plt.plot(freq, amp)
-        #     plt.annotate(str(int(hf))+"Hz", xy=(hf, max(amp)))
-
-        # hf = best
-
-        # if debug:
-        #     if lf == 0 or hf == 0:
-        #         txt = "Unknown dial tone"
-        #     else:
-        #         txt = str(lf)+"Hz + "+str(hf)+"Hz -> "+dtmf[(lf,hf)]
-        #     plt.xlabel(txt)
-
-
-        # t = int(i//step * precision)
-
-        # if verbose and t > int((i-1)//step * precision):
-        #     m = str(int(t//60))
-        #     s = str(t%60)
-        #     s = "0"*(2-len(s)) + s
-        #     print ("\n"+m+":"+s+" ", end='', flush=True)
-
-        # if lf == 0 or hf == 0:
-        #     if verbose:
-        #         print(".", end='', flush=True)
-        #     c = ""
-        # elif dtmf[(lf,hf)] != c or verbose:
-        #     c = dtmf[(lf,hf)]
-        #     print(c, end='', flush=True)
+        found_frequencies.append(best)
 
         if debug:
             plt.show()
 
-    print()
+    return found_frequencies
 
-except KeyboardInterrupt:
-    print("\nCTRL+C detected: exiting...")
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Extract phone numbers from an audio recording of the dial tones.")
+    parser.add_argument("-d", "--debug", help="show graphs to debug", action="store_true")
+    parser.add_argument('file', type=argparse.FileType('r'))
+
+    args = parser.parse_args()
+
+    fps, data = wavfile.read(args.file.name)
+    fps2, data2 = wavfile.read("0.wav")
+    print(get_freq(data2))
+    print(get_freq_seq(data, args.debug))
