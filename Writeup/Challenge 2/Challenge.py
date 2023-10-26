c8e2660206786849b7b351bbe07e206ee2afcfd3
Adam <henault.adam@gmail.com>
2023-10-26 20:12:09 +0200

    Challenge 2

--- Writeup/Challenge 2/Challenge.py
+++ Writeup/Challenge 2/Challenge.py
@@ -2,46 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 import argparse
-from collections import Counter
 
-
-sample_freq = {1722 : "0", 953 : "1", 1012 : "2", 1063 : "3", 1187 : "4", 1247 : "5", 1305 : "6", 1429 : "7", 1486 : "8", 1546 : "9", 1131 : "A", 1363 : "B", 1606 : "C", 1835 : "D", 1664: "E"}
-
-def get_freq(data):
-
-    found_frequencies = []
-
-    duration = len(data)/fps
-    step = int(len(data)//(duration//0.1))
-
-    for i in range(0, len(data)-step, step):
-        signal = data[i:i+step]
-        
-        frequencies = np.fft.fftfreq(signal.size, d=1/fps)
-        amplitudes = np.fft.fft(signal)
-
-        # Low
-        i_min = np.where(frequencies > 50)[0][0]
-        i_max = np.where(frequencies > 2000)[0][0]
-        
-        freq = frequencies[i_min:i_max]
-        amp = abs(amplitudes.real[i_min:i_max])
-
-        lf = freq[np.where(amp == max(amp))[0][0]]
-
-        delta = 20
-        best = 0
-
-        for f in sample_freq:
-            if abs(lf-f) < delta:
-                delta = abs(lf-f)
-                best = f
-
-        found_frequencies.append(best)
-
-    frequencies_count = Counter(found_frequencies)
-    found_freq, _ = frequencies_count.most_common(1)[0]
-    return found_freq
+sample_freq = {1722 : "0", 953 : "1", 1012 : "2", 1063 : "3", 1187 : "4", 1247 : "5", 1305 : "6", 1429 : "7", 1486 : "8", 1540 : "9", 1131 : "A", 1363 : "B", 1606 : "C", 1835 : "D", 1664: "*"}
 
 def get_freq_seq(data, debug):
 
@@ -89,6 +51,7 @@ def get_freq_seq(data, debug):
             plt.title("Fourier transform")
             plt.plot(freq, amp)
             plt.annotate(str(int(lf))+"Hz", xy=(lf, max(amp)))
+            plt.yticks([])
 
         found_frequencies.append(best)
 
@@ -96,6 +59,46 @@ def get_freq_seq(data, debug):
             plt.show()
 
     return found_frequencies
+
+def regroup_freq(freqs):
+
+    if not freqs:
+        return []
+
+    resultat = []
+    count = 1
+
+    for i in range(1, len(freqs)):
+        
+        if freqs[i] != freqs[i - 1] and freqs[i] != 0 and freqs[i-1] != 0:
+            
+            if freqs[i-1] == 0:
+                pass
+
+            if count >= 8:
+                resultat.append(freqs[i-1])
+                resultat.append(freqs[i-1])
+                count = 1
+            else:
+                resultat.append(freqs[i-1])
+                count = 1
+        else:
+            count += 1
+
+    for i in range(len(freqs) - 1, -1, -1):
+        if freqs[i] != 0:
+            resultat.append(freqs[i])
+            break
+
+    return resultat
+        
+def get_key_from_freqs(freqs):
+
+    key = ""
+    for freq in freqs:
+        key += sample_freq[freq]
+
+    return key
 
 if __name__ == '__main__':
 
@@ -106,6 +109,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     fps, data = wavfile.read(args.file.name)
-    fps2, data2 = wavfile.read("0.wav")
-    print(get_freq(data2))
-    print(get_freq_seq(data, args.debug))
+    freqs = get_freq_seq(data, args.debug)
+
+    final_freqs = regroup_freq(freqs)
+
+    key = get_key_from_freqs(final_freqs)
+    print(key)
